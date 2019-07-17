b91369dd17a940c4c84e5f746ebaab507389574c
sharky <zeebing@gmail.com>
2019-07-17 14:21:15 -0400

    fresh from react-portfolio

--- src/components/Project/Tech.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Tech(props) {
-    return(
-        <i class={props.theme==="light" ? props.logo[1] : props.logo[0]} title={props.title}></i>
-        // <img src={props.src} alt={props.alt} title={props.title}></img>
-    )
-}
-export default Tech;
